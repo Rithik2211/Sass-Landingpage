8911fe11d4921f3b8a6df8943f3abbc79f9a49bf
Rithik2211 <rithikpranao22@gmail.com>
2024-08-30 10:38:12 +0530

    initial setup commit

---
+++ sass-landingpage/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+import "./globals.css";
+import { DM_Sans } from "next/font/google";
+import {twMerge} from 'tailwind-merge';
+
+const dmsans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sass landing Page",
+  description: "Created by Rithik",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={twMerge(dmsans.className, "antialized bg-[#EAEEFE]")}>{children}</body>
+    </html>
+  );
+}
