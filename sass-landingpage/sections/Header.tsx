0db4c6f30f87da6a53d16f3b88e7887de8a685aa
Rithik2211 <rithikpranao22@gmail.com>
2024-09-11 22:41:21 +0530

    Added a new `<h1>` element with the text "new component" to the `Header` component.

--- sass-landingpage/sections/Header.tsx
+++ sass-landingpage/sections/Header.tsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <div className='flex justify-center items-center py-3 bg-black text-white'>
         <p> Get Started For Free</p>
+        <h1>new component</h1>
     </div>
   )
 }
